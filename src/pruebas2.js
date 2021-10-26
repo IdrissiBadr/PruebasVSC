aec0956bf14035a31e56ea4fb4dbe7c954bccfc1
IdrissiBadr <bidr932@g.educaand.es>
2021-10-26 13:40:41 +0200

    ApuntesArray

--- src/pruebas2.js
+++ src/pruebas2.js
@@ -1,12 +1,12 @@
  db.inventory.drop()
  db.inventory.insertMany(
     [
-        { item: "journal", qty: 25, tags: ["blank", "red"], dim_cm: [ 14, 21 ] },
-        { item: "notebook", qty: 50, tags: ["red", "blank"], dim_cm: [ 14, 21 ] },
-        { item: "paper", qty: 100, tags: ["red", "blank", "plain"], dim_cm: [ 14, 21 ] },
-        { item: "planner", qty: 75, tags: ["blank", "red"], dim_cm: [ 22.85, 30 ] },
-        { item: "postcard", qty: 45, tags: ["blue"], dim_cm: [ 10, 15.25 ] },
-        { item: "postcard", qty: 45, tags: "red", dim_cm: [ 10, 15.25 ] },
+        { item: "journal", qty: 25, tags: ["blank", "red"], dim_cm: [ 14, 21 ], dateA: Date() },
+        { item: "notebook", qty: 50, tags: ["red", "blank"], dim_cm: [ 14, 21 ], dateA: Date("2020-02-15") },
+        { item: "paper", qty: 100, tags: ["red", "blank", "plain"], dim_cm: [ 14, 21 ], dateA: new Date() },
+        { item: "planner", qty: 75, tags: ["blank", "red"], dim_cm: [ 22.85, 30 ], dateA: new Date("2020-03-20") },
+        { item: "postcard", qty: 45, tags: ["blue"], dim_cm: [ 10, 15.25 ], dateA: new Date("2020-05-25") },
+        { item: "postcard", qty: 45, tags: "red", dim_cm: [ 10, 15.25 ], dateA: new Date("2020-02-30") },
     ]
  );
   db.inventory.find( { tags: ["red", "blank"] } )

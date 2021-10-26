db86d906e72cdccef640e1e42128ef3d3b03ea90
IdrissiBadr <bidr932@g.educaand.es>
2021-10-26 14:26:21 +0200

    ApuntesFecha

--- src/pruebas3Fechas.js
+++ src/pruebas3Fechas.js
@@ -4,7 +4,7 @@ db.fechas.insertMany(
         {año: 1999, mes: 2, dia: 18},
         {año: 2007, mes: 6, dia: 29},
         {año: 2021, mes: 2, dia: 1},
-        {}
+        {año: 2017, mes: 12, dia: 24},
     ]
 
 );
